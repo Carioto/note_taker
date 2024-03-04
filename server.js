978c0fd6e7ab635b9faec66294e160c53f80f50a
DaveC <dcarioto114@outlook.com>
2024-03-03 19:45:27 -0500

    fix viewing new note

--- server.js
+++ server.js
@@ -13,13 +13,30 @@ app.use(express.static('public'));
 app.get('/notes', (req,res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-  
-app.get('/api/notes', (req,res) => 
-  res.json(noteData));
+ //every time you go to api/notes the page is reloaded
+ //with this code, unlike  
+app.get('/api/notes', (req,res) => {
+fs.readFile('./db/db.json', 'utf-8')
+.then((data) => {
+    notes = JSON.parse(data);
+    res.json(notes);
+});
+});
+
+app.get('api/notes/:id', (req,res) => {
+    activeNote = req.params.id;
+    console.log(activeNote);
+    fs.readFile('./db/db.json', 'utf-8')
+    .then ((data) =>{
+      const allNotes = JSON.parse(data);
+      res.json(allNotes[req.params.id]);
+    })
+});
+
 
 app.post('/api/notes', (req,res) => {
     const addNote = {
-        note_id:uuid(),
+        id:uuid(),
         title:req.body.title,
         text: req.body.text,
     };
