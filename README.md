ca66bb5177e815449f93e192496897a1b2ba3bb4
DaveC <dcarioto114@outlook.com>
2024-03-03 20:45:02 -0500

    add view note, delete note, README, and walkthrough

---
+++ README.md
@@ -0,0 +1,51 @@
+# Note Taker
+![License:MIT](https://img.shields.io/badge/License-MIT-blue)
+
+## Description
+Collect notes with titles and descriptions that can be revisited and deleted
+
+## Table of Contents
+
+-[Installation](#installation)
+
+-[Usage](#usage)
+
+-[Credits](#credits)
+
+-[License](#license)
+
+-[Tests](#tests)
+
+-[Questions](#questions)
+
+
+
+## Installation
+
+In this application's working directory, run 'npm i' prior to first use
+
+## Usage
+
+After installation, begin the application with 'node server.js'
+
+## License
+
+This software is covered under the following license:
+MIT No Attribution License
+        [View license](https://opensource.org/license/mit-0/)
+
+## Credits
+
+Github users amandalatkins and jfisher396 approaches were referenced in the creation of this application.
+
+## Tests
+
+Notes are generated and deleted in the test case seen in the video walkthrough
+
+[Video Walkthrough](https://drive.google.com/file/d/1YHzN-qP-IIt6Gv2EaUqPShYSMqVz3xKn/view)
+
+## Questions
+
+Contact me by email at dcarioto114@outlook.com
+
+or visit my GitHub repository: [github.com/Carioto](https://github.com/Carioto)
--- server.js
+++ server.js
@@ -7,53 +7,58 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/notes', (req,res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html')));
 
- //every time you go to api/notes the page is reloaded
- //with this code, unlike  
-app.get('/api/notes', (req,res) => {
-fs.readFile('./db/db.json', 'utf-8')
-.then((data) => {
-    notes = JSON.parse(data);
-    res.json(notes);
-});
-});
-
-app.get('api/notes/:id', (req,res) => {
-    activeNote = req.params.id;
-    console.log(activeNote);
+//every time you go to api/notes the page is reloaded
+//with this code, unlike  
+app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8')
-    .then ((data) =>{
-      const allNotes = JSON.parse(data);
-      res.json(allNotes[req.params.id]);
-    })
+        .then((data) => {
+            notes = JSON.parse(data);
+            res.json(notes);
+        });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    activeNote = req.params.id;
+    fs.readFile('./db/db.json', 'utf-8')
+        .then((data) => {
+            const allNotes = JSON.parse(data);
+            const newNoteListA = allNotes.filter((note) =>
+                note.id !== activeNote);
+            const newNoteList = JSON.stringify(newNoteListA);
+            fs.writeFile('./db/db.json', newNoteList, (err) =>
+                err ? console.error(err) : console.log(`note deleted from database`)
+            );
+            res.json(newNoteList);
+        })
 });
 
 
-app.post('/api/notes', (req,res) => {
+app.post('/api/notes', (req, res) => {
     const addNote = {
-        id:uuid(),
-        title:req.body.title,
+        id: uuid(),
+        title: req.body.title,
         text: req.body.text,
     };
-    
+
     fs.readFile('./db/db.json', 'utf-8')
-    .then((data) => {
-        notes = JSON.parse(data);
-        notes.push(addNote);
-        updatedNotes= JSON.stringify(notes);
-        fs.writeFile('./db/db.json',updatedNotes, (err) =>
-        err ? console.error(err) : console.log(`note written to database`)
-        );
-        res.json(notes);
-    });
+        .then((data) => {
+            notes = JSON.parse(data);
+            notes.push(addNote);
+            updatedNotes = JSON.stringify(notes);
+            fs.writeFile('./db/db.json', updatedNotes, (err) =>
+                err ? console.error(err) : console.log(`note written to database`)
+            );
+            res.json(notes);
+        });
 });
-app.get('*', (req,res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html")));
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, "./public/index.html")));
 
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`)
